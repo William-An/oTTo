dc3a221386d4309aba519204d80e84a8f84f617e
William-An <China_Aisa@live.com>
2021-09-07 16:32:43 -0400

    Add imu icm 20948 register addr

--- components/imu_sensor/include/imu_icm_20948.h
+++ components/imu_sensor/include/imu_icm_20948.h
@@ -11,8 +11,88 @@
 *
 ************************************************************/
 
-#include "imu_sensor.h"
+/**
+    Software License Agreement (BSD License)
+
+    Copyright (c) 2019 Bryan Siepert for Adafruit Industries
+    All rights reserved.
+
+    Redistribution and use in source and binary forms, with or without
+    modification, are permitted provided that the following conditions are met:
+    1. Redistributions of source code must retain the above copyright
+    notice, this list of conditions and the following disclaimer.
+    2. Redistributions in binary form must reproduce the above copyright
+    notice, this list of conditions and the following disclaimer in the
+    documentation and/or other materials provided with the distribution.
+    3. Neither the name of the copyright holders nor the
+    names of its contributors may be used to endorse or promote products
+    derived from this software without specific prior written permission.
+
+    THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS ''AS IS'' AND ANY
+    EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+    WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+    DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER BE LIABLE FOR ANY
+    DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+    (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+    LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+    ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+    (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+    SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
 #include <inttypes.h>
+#include "driver/i2c.h"
+#include "imu_sensor.h"
+#include "imu_icm_20x.h"
+
+#define ICM20948_I2CADDR_DEFAULT 0x69 ///< ICM20948 default i2c address
+#define ICM20948_MAG_ID 0x09          ///< The chip ID for the magnetometer
+
+#define ICM20948_UT_PER_LSB 0.15 ///< mag data LSB value (fixed)
+
+#define AK09916_WIA2 0x01  ///< Magnetometer
+#define AK09916_ST1 0x10   ///< Magnetometer
+#define AK09916_HXL 0x11   ///< Magnetometer
+#define AK09916_HXH 0x12   ///< Magnetometer
+#define AK09916_HYL 0x13   ///< Magnetometer
+#define AK09916_HYH 0x14   ///< Magnetometer
+#define AK09916_HZL 0x15   ///< Magnetometer
+#define AK09916_HZH 0x16   ///< Magnetometer
+#define AK09916_ST2 0x18   ///< Magnetometer
+#define AK09916_CNTL2 0x31 ///< Magnetometer
+#define AK09916_CNTL3 0x32 ///< Magnetometer
+
+/** The accelerometer data range */
+typedef enum {
+  ICM20948_ACCEL_RANGE_2_G,
+  ICM20948_ACCEL_RANGE_4_G,
+  ICM20948_ACCEL_RANGE_8_G,
+  ICM20948_ACCEL_RANGE_16_G,
+} icm20948_accel_range_t;
+
+/** The gyro data range */
+typedef enum {
+  ICM20948_GYRO_RANGE_250_DPS,
+  ICM20948_GYRO_RANGE_500_DPS,
+  ICM20948_GYRO_RANGE_1000_DPS,
+  ICM20948_GYRO_RANGE_2000_DPS,
+} icm20948_gyro_range_t;
+
+/**
+ * @brief Data rates/modes for the embedded AsahiKASEI AK09916 3-axis
+ * magnetometer
+ *
+ */
+typedef enum {
+  AK09916_MAG_DATARATE_SHUTDOWN = 0x0, ///< Stops measurement updates
+  AK09916_MAG_DATARATE_SINGLE =
+      0x1, ///< Takes a single measurement then switches to
+           ///< AK09916_MAG_DATARATE_SHUTDOWN
+  AK09916_MAG_DATARATE_10_HZ = 0x2,  ///< updates at 10Hz
+  AK09916_MAG_DATARATE_20_HZ = 0x4,  ///< updates at 20Hz
+  AK09916_MAG_DATARATE_50_HZ = 0x6,  ///< updates at 50Hz
+  AK09916_MAG_DATARATE_100_HZ = 0x8, ///< updates at 100Hz
+} ak09916_data_rate_t;
 
 class ICM20948IMU : public GenericIMU {
     public:
@@ -25,13 +105,27 @@ class ICM20948IMU : public GenericIMU {
         void updateGyro();
         void updateMagnet();
 
+        // Use selfTest functionality
+        void selfTest();
+
+        // Configure the sensor
+        void config();
+
+        // Begin transcation with the sensor
+        void begin();
+
         // Mock testing methods
         void updateMockAccel(Vector3_t);
         void updateMockGyro(Vector3_t);
         void updateMockMagnet(Vector3_t);
 
+        // Temperature sensor getter and setter
+        void updateTemp();
+        float getTemp();
+
     private:
         // TODO Might need some member fields here
+        float temp;
 };
 
 
