5fde8a3e91bb425fc22200a74701805c6562f87d
Richard <zhangruichao2000@gmail.com>
2021-09-09 17:11:23 -0400

    Add stepper motor driver header file

---
+++ components/motor_driver/include/motor_stepper.h
@@ -0,0 +1,22 @@
+#ifndef __MOTOR_STEPPER__
+#define __MOTOR_STEPPER__
+
+/************************************************************
+*
+* @file:      motor_stepper.h
+* @author:    Ruichao Zhang
+* @email:     zhan3147@purue.edu
+* @version:   v1.0.0
+* @date:      09/09/2021
+* @brief:     NEMA17 stepper(1.8 degree) with A4988 Driver
+*
+************************************************************/
+
+#include "motor_driver.h"
+#include <inttypes.h>
+
+// class A4988_Driver : public GenericMotorDriver {
+    
+// };
+
+#endif
