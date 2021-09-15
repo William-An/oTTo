6218f92a561fb4de2f4f2882d2d31e8f48458954
William-An <China_Aisa@live.com>
2021-09-15 17:59:59 -0400

    Add license

--- components/lcd1602/lcd1602.cpp
+++ components/lcd1602/lcd1602.cpp
@@ -1,6 +1,31 @@
 #include <stdio.h>
 #include "lcd1602.h"
 
+// Adapted from https://github.com/DavidAntliff/esp32-i2c-lcd1602
+/*
+ * MIT License
+ *
+ * Copyright (c) 2018 David Antliff
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
 LCD1602::LCD1602(uint8_t addr) {
     mcp23008_addr = addr;
 }
@@ -77,6 +102,7 @@ esp_err_t LCD1602::write_string(const char *str) {
     return ESP_OK;
 }
 
+// Refer to ICM 20948 for documentation
 esp_err_t LCD1602::readReg(uint8_t regAddr, uint8_t* data, size_t len) {
 
     // Write then read sequence for ICM 20948, 
