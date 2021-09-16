3cdf8b8d61afe8c5efd619dcac3c30e2f9a5280b
William-An <China_Aisa@live.com>
2021-09-16 16:40:50 -0400

    Add some comments on methods

--- components/communication_if/uart_wired.cpp
+++ components/communication_if/uart_wired.cpp
@@ -18,12 +18,18 @@ UartWired :: UartWired(bool debugMode) {
     // We won't use a buffer for sending data.
     uart_driver_install(UART_NUM_0, RX_BUF_SIZE * 2, 0, 0, NULL, 0);
     uart_param_config(UART_NUM_0, &uart_config);
+
+    // TODO Weili: Use #define for the pin num?
     uart_set_pin(0, 1, 3, -1, -1);
 }
 
+// TODO Weili: Use uint8_t instead of char, and add size_t len
+// TODO Weili: As the data we send might not be cstring
 int UartWired :: sendData(const char* data) {
     const int len = strlen(data);
     const int txBytes = uart_write_bytes(UART_NUM_0, data, len);
+    // TODO Weili: You can use `ESP_LOGD` to log debugging outputs
+    // TODO Weili: Similarly for warning, can use `ESP_LOGW`
     if (_debugMode) {
         static const char *TX_TASK_TAG = "TX_TASK";
         esp_log_level_set(TX_TASK_TAG, ESP_LOG_INFO);
